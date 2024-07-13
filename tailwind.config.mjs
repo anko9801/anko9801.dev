8a1b7c44c1e5852c20bbbd8fc60e52b212c326d2
anko9801 <daiusa3@icloud.com>
2024-07-14 05:34:34 +0900

    🎨 Tailwind jit mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		animation: {
-			"slide-in-bottom":
-				"slide-in-bottom 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
-		},
-		keyframes: {
-			"slide-in-bottom": {
-				"0%": {
-					scale: "0",
-					opacity: "1",
-					translate: "-50% -50%"
-				},
-				"100%": {
-					scale: "1",
-					opacity: "0",
-					translate: "-50% -50%"
-				},
-			},
-		},
-	},
-	plugins: [],
+  mode: 'jit',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    animation: {
+      'slide-in-bottom':
+        'slide-in-bottom 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+    },
+    keyframes: {
+      'slide-in-bottom': {
+        '0%': {
+          scale: '0',
+          opacity: '1',
+          translate: '-50% -50%',
+        },
+        '100%': {
+          scale: '1',
+          opacity: '0',
+          translate: '-50% -50%',
+        },
+      },
+    },
+  },
+  plugins: [],
 }
